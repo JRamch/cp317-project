d406240131c51efb36868a20668db8b4fa582b68
JoshGelbaum <161539595+JoshGelbaum@users.noreply.github.com>
2024-11-19 18:42:45 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,28 @@
  * Authors: Josh Gelbaum, Drake Martin, Paul Matsialko, Jaden Ramcharan
  */
 
-import java.io.IOException;
+ import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            FileProcessor processor = new FileProcessor();
-
-            //Paths
-            String nameFilePath = "NameFile.txt";
-            String courseFilePath = "CourseFile.txt";
-            String outputFilePath = "OutputFile.txt";
-
-            //Process files
-            processor.readFiles(nameFilePath, courseFilePath);
-            processor.writeOutputFile(outputFilePath);
-
-
-            System.out.println("Done! Output written to " + outputFilePath);
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
-        }
-    }
-}
+ public class Main {
+     public static void main(String[] args) {
+         try {
+             FileProcessor processor = new FileProcessor();
+ 
+             //Paths
+             String nameFilePath = "NameFile.txt";
+             String courseFilePath = "CourseFile.txt";
+             String outputFilePath = "OutputFile.txt";
+ 
+             //Process files
+             processor.readFiles(nameFilePath, courseFilePath);
+             processor.writeOutputFile(outputFilePath);
+ 
+             System.out.println("Done! Output written to " + outputFilePath);
+         } catch (IOException e) {
+             System.err.println("Error: " + e.getMessage());
+         } catch (IllegalArgumentException e) {
+             System.err.println("Validation Error: " + e.getMessage());
+         }
+     }
+ }
+ 
