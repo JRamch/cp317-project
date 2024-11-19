9fd90bd3cbff03f2e46ef01b3bb17fa25bddca03
JoshGelbaum <161539595+JoshGelbaum@users.noreply.github.com>
2024-11-19 13:03:07 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,26 @@
  * Authors: Josh Gelbaum, Drake Martin, Paul Matsialko, Jaden Ramcharan
  */
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        try {
+            FileProcessor processor = new FileProcessor();
+
+            //Paths
+            String nameFilePath = "NameFile.txt";
+            String courseFilePath = "CourseFile.txt";
+            String outputFilePath = "OutputFile.txt";
+
+            //Process files
+            processor.readFiles(nameFilePath, courseFilePath);
+            processor.writeOutputFile(outputFilePath);
+
+
+            System.out.println("Done! Output written to " + outputFilePath);
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
