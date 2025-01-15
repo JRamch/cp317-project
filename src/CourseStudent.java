08e78a7e35447e4406b8581cf2a53b1a8d20c316
paulmatsialko <paulmatsialkoo@gmail.com>
2025-01-15 18:20:33 -0500

    Update CourseStudent.java

--- src/CourseStudent.java
+++ src/CourseStudent.java
@@ -26,39 +26,39 @@
     public String getCourseCode() {
         return courseCode;
     }
-
+    // Getter for test1
     public void setCourseCode(String courseCode) {
         this.courseCode = courseCode;
     }
-
+    // Getter for test1
     public double getTest1() {
         return test1;
     }
-
+     // setter for test1
     public void setTest1(double test1) {
         this.test1 = test1;
     }
-
+    // getter for test2
     public double getTest2() {
         return test2;
     }
-
+    // setter for test2
     public void setTest2(double test2) {
         this.test2 = test2;
     }
-
+     // getter for test3
     public double getTest3() {
         return test3;
     }
-
+     // setter for test3
     public void setTest3(double test3) {
         this.test3 = test3;
     }
-
+    // getter for final exam score
     public double getFinalExam() {
         return finalExam;
     }
-
+    // Setter for final exam score
     public void setFinalExam(double finalExam) {
         this.finalExam = finalExam;
     }
